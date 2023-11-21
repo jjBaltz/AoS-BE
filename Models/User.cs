5319a96221266845b50067a36f8b7ece86013dcf
Jasper Baltz <nickolas.baltz@gmail.com>
2023-11-20 21:11:11 -0600

    changed nullable User Model

--- Models/User.cs
+++ Models/User.cs
@@ -4,10 +4,10 @@
     {
         public int UserId { get; set; }
         public string UID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string ImageUrl { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName{ get; set; }
+        public string? Email { get; set; }
+        public string? ImageUrl { get; set; }
         public List <Activity>? Activities { get; set; }
         public List <Memory>? Memories { get; set; }
     }
